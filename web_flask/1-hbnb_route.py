9ef5b2b771c30e2dfd4afdf22c611db279ea2837
HichamELMaa <hm.elmaataoui@gmail.com>
2023-10-22 11:49:52 -0700

    web framework

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""web application must be listening on 0.0.0.0"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def Hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
